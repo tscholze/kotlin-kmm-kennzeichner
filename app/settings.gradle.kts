775f7f6b22df8150513aa4c11acbf61262d48321
Tobias Scholze <tobias.scholze+git@me.com>
2023-03-10 18:47:51 +0100

    Project wizard run
    re #1

---
+++ app/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Kennzeichner"
+include(":androidApp")
+include(":shared")
