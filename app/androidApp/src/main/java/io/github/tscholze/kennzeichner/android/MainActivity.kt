bb29937c3dcc0ebb309e6b0834f87d0647bc00ae
Tobias Scholze <t.scholze@anfe.ma>
2023-03-30 11:02:25 +0200

    Added ktor setup to the project

--- app/androidApp/src/main/java/io/github/tscholze/kennzeichner/android/MainActivity.kt
+++ app/androidApp/src/main/java/io/github/tscholze/kennzeichner/android/MainActivity.kt
@@ -4,11 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import io.github.tscholze.kennzeichner.Greeting
+import io.github.tscholze.kennzeichner.data.LicensePlateRepository
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,22 +26,22 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    GreetingView(Greeting().greet())
+                    val scope = rememberCoroutineScope()
+                    var text by remember { mutableStateOf("") }
+
+                    LaunchedEffect(true) {
+                        scope.launch {
+                            text = try {
+                               LicensePlateRepository().demo()
+                            } catch (e: Exception) {
+                                e.localizedMessage ?: "error"
+                            }
+                        }
+                    }
+
+                    Text(text = text)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun GreetingView(text: String) {
-    Text(text = text)
-}
-
-@Preview
-@Composable
-fun DefaultPreview() {
-    MyApplicationTheme {
-        GreetingView("Hello, Android!")
     }
 }
