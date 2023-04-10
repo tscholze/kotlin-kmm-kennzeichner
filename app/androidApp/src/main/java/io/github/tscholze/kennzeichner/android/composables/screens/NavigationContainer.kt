cfeee5f7968a9a863a0855a31cceb80b0902899b
Tobias Scholze <tobias.scholze+git@me.com>
2023-04-10 11:56:20 +0200

    Introduced sealed class for bottom navigation
    re #10

--- app/androidApp/src/main/java/io/github/tscholze/kennzeichner/android/composables/screens/NavigationContainer.kt
+++ app/androidApp/src/main/java/io/github/tscholze/kennzeichner/android/composables/screens/NavigationContainer.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import io.github.tscholze.kennzeichner.android.composables.components.BottomNavigationItemData
 import io.github.tscholze.kennzeichner.android.composables.screens.regions.list.RegionsScreen
 
 /**
@@ -19,18 +20,18 @@ fun NavigationContainer() {
 
     NavHost(navController = navController, startDestination = "regions") {
         // Regions
-        composable("regions") {
+        composable(BottomNavigationItemData.List.route) {
             RegionsScreen(navController)
         }
 
         // Region's detail
-        composable("regions/{id}") { backStackEntry ->
+        composable("${BottomNavigationItemData.List.route}/{id}") { backStackEntry ->
             val id = backStackEntry.arguments?.getString("id") ?: "A"
             RegionScreen(regionId = id, navController)
         }
 
         // Map
-        composable("map") {
+        composable(BottomNavigationItemData.Map.route) {
             MapScreen(navController)
         }
     }
