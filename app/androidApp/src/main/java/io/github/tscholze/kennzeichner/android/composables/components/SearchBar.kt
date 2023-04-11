5eca7e7b454c87c3ab3216634eac21a8031c755d
Tobias Scholze <tobias.scholze+git@me.com>
2023-04-11 16:01:58 +0200

    Introduced view models and refined name spacing to the android app
    re #10

--- app/androidApp/src/main/java/io/github/tscholze/kennzeichner/android/composables/components/SearchBar.kt
+++ app/androidApp/src/main/java/io/github/tscholze/kennzeichner/android/composables/components/SearchBar.kt
@@ -1,8 +1,12 @@
 package io.github.tscholze.kennzeichner.android.composables.components
 
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
@@ -10,17 +14,38 @@ import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import io.github.tscholze.kennzeichner.android.R
 
+/**
+ * Represents a search bar.
+ * If the user types or clears the search bar text the state
+ * property will be updated.
+ *
+ * @param state: Observable state of the search query.
+ */
 @Composable
 fun SearchBar(state: MutableState<String>) {
+    // MARK: - Properties -
+
+    val focusManager = LocalFocusManager.current
+
+    // MARK: - UI -
+
     TextField(
         modifier = Modifier.fillMaxWidth(),
         value = state.value,
-        onValueChange = {
-            print(it)
-            state.value = it
-                        },
+        onValueChange = { state.value = it },
         singleLine = true,
+        placeholder = {
+            Text(
+                stringResource(R.string.searchbar_text_placeholder),
+                color = MaterialTheme.colors.onSecondary
+            )
+        },
         leadingIcon = {
             Icon(
                 Icons.Default.Search,
@@ -29,7 +54,12 @@ fun SearchBar(state: MutableState<String>) {
         },
         trailingIcon = {
             if (state.value.isNotEmpty()) {
-                IconButton(onClick = { state.value = "" }) {
+                IconButton(
+                    onClick = {
+                        state.value = ""
+                        focusManager.moveFocus(FocusDirection.Next)
+                    }
+                ) {
                     Icon(
                         Icons.Default.Close,
                         contentDescription = "Clear icon",
@@ -37,5 +67,12 @@ fun SearchBar(state: MutableState<String>) {
                 }
             }
         },
+        keyboardActions = KeyboardActions(
+            onAny = { focusManager.clearFocus() }
+        ),
+        keyboardOptions = KeyboardOptions(
+            autoCorrect = false,
+            imeAction = ImeAction.Search
+        )
     )
 }
